3c60dce8911d2ed80ae90f7a5c1b8cfaea80f0a0
Chris Woodward <cwoodward@narrative.io>
2020-03-17 15:10:14 -0700

    Created text for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MeetingsComponent } from './components/pages/meetings/meetings.component';
+import { LinksComponent } from './components/pages/links/links.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "home",
+		component: HomeComponent
+	},
+	{
+		path: "meetings",
+		component: MeetingsComponent
+	},
+	{
+		path: "links",
+		component: LinksComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { SidenavListComponent } from './components/nav/sidenav-list/sidenav-list.component';
 import { HeaderComponent } from './components/nav/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MeetingsComponent } from './components/pages/meetings/meetings.component';
+import { LinksComponent } from './components/pages/links/links.component';
 
 @NgModule({
   declarations: [
 	AppComponent,
 	SidenavListComponent,
-	HeaderComponent
+	HeaderComponent,
+	HomeComponent,
+	MeetingsComponent,
+	LinksComponent
   ],
   imports: [
     BrowserModule,
